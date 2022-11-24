15cb6cb2a467ddecad1628ebd5876e1243dd1b4a
happyPrince169 <37725072+happyPrince169@users.noreply.github.com>
2022-11-24 20:14:02 +0000

    add datasets and scripts

---
+++ models/train_classifier.py
@@ -0,0 +1,107 @@
+import sys
+import pandas as pd
+from sqlalchemy.engine import create_engine
+from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer, TfidfVectorizer
+from sqlalchemy import create_engine
+
+import nltk
+nltk.download(['punkt', 'wordnet', 'averaged_perceptron_tagger'])
+
+from nltk.tokenize import word_tokenize
+from nltk.tokenize import sent_tokenize
+from nltk.stem import WordNetLemmatizer
+import re
+import pickle
+
+from sklearn.metrics import accuracy_score
+import numpy as np
+from sklearn import metrics
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+def load_data(database_filepath):
+    engine = create_engine('sqlite:///'+ database_filepath)
+    df = pd.read_sql_table('DisasterResponse', engine)
+    # Column child_alone has only 0 value
+    df = df.drop('child_alone', axis = 1)
+    # Remove rows where value of related column is 2 as it seems to be an error
+    df = df[df['related'] != 2]
+    X = df.message
+    y = df.iloc[:,4:]
+    category_names = y.columns
+    return X, y, category_names
+
+
+def tokenize(text):
+    regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    urls = re.findall(regex, text)
+    for url in urls:
+        text = text.replace(url, "urlplaceholder")
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+    clean_tokens = []
+    for t in tokens:
+        clean_tok = lemmatizer.lemmatize(t).lower().strip()
+        clean_tokens.append(clean_tok)
+    return clean_tokens
+
+
+def build_model():
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier()))
+    ])
+    return pipeline
+
+
+def evaluate_model(model, X_test, Y_test, category_names):
+    y_pred = model.predict(X_test)
+    report = classification_report(Y_test, y_pred, target_names=category_names)
+    print(report)
+
+
+def save_model(model, model_filepath):
+    with open(model_filepath, 'wb') as file:
+        pickle.dump(model, file)
+
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+        
+        print('Building model...')
+        model = build_model()
+        
+        print('Training model...')
+        model.fit(X_train, Y_train)
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
+
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()

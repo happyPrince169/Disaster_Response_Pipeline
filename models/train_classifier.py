1af72895dadf397ff09638944f79207e6cd071ca
happyPrince169 <37725072+happyPrince169@users.noreply.github.com>
2022-11-25 03:38:16 +0000

    using GridSearchCV

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -65,7 +65,7 @@ def build_model():
         'clf__estimator__n_estimators': [10],
         'clf__estimator__min_samples_split': [2],
     }
-    model = GridSearchCV(pipeline, param_grid=parameters, n_jobs=4, verbose=2, cv=3)
+    model = GridSearchCV(pipeline, param_grid=parameters)
     return model
 
 
